3a4262743a770339228c6ddd0d902e659f240c97
alexey novik <alexnovikdev@gmail.com>
2019-04-30 14:10:16 +0300

    add resize to canvas

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,26 @@
-let type = "WebGL";
+let app = new PIXI.Application({
+    width: 256, 
+    height: 256,
+    antialias: true,
+    transparent: false,
+    resolution: 1
+});
 
-if (!PIXI.utils.isWebGLSupported()) {
-    type = "canvas"
+document.body.appendChild(app.view);
+
+app.renderer.backgroundColor = 0x000000;
+
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
+
+window.addEventListener("resize", (event) => {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+    onSizeChange();
+});
+
+function onSizeChange() {
+    
 }
-
-PIXI.utils.sayHello(type);
